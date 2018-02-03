9b5d13b702af3d6c42f2bc8d7c6f29fd44f7cb52
Dan Allen <dan.j.allen@gmail.com>
2018-02-02 23:42:43 -0700

    add additional configuration and integrations

--- template/config.rb
+++ template/config.rb
@@ -1,5 +1,3 @@
-require 'middleman-asciidoc'
-
 # = Site properties
 # See data/site.yml for seed values
 ready do
@@ -41,6 +39,7 @@ end
 page '/*.xml', layout: false
 page '/*.json', layout: false
 page '/*.txt', layout: false
+#page '/', layout: 'home'
 
 # Page with alternative layout
 # page '/path/to/file.html', layout: 'other_layout'
@@ -55,11 +54,19 @@ helpers do
 end
 =end
 
-# = Build-specific configuration
+# NOTE: The `server` command defaults to the :development environment.
+configure :development do
+  activate :livereload if ENV['LIVE_RELOAD'] == 'true'
+end
+
+# = Production build-specific configuration
 # See https://middlemanapp.com/advanced/configuration/#environment-specific-settings
-=begin
-configure :build do
+# NOTE The `build` command defaults to the :production environment.
+configure :production do
+  set :http_prefix, (ENV.fetch 'HTTP_PREFIX', '/')
+  config.slim[:indent] = ''
+  activate :autoprefixer
   activate :minify_css
   activate :minify_javascript
+  #activate :google_analytics, tracking_id: 'UA-XXXXXXXX-1', minify: true
 end
-=end
